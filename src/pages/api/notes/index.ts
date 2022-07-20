f128e4c58f7e77a82219128d44e0aece44cc21e9
ojosdegatonegro <diegofernandocubides@gmail.com>
2022-07-20 18:40:45 -0500

    Creates a note

--- src/pages/api/notes/index.ts
+++ src/pages/api/notes/index.ts
@@ -1,13 +1,18 @@
 import {NextApiRequest, NextApiResponse} from 'next';
+import {connectDB} from './../../../utils/database';
 
-function notes(req: NextApiRequest, res: NextApiResponse)  {
-  const {method} = req;
+async function notes(req: NextApiRequest, res: NextApiResponse)  {
+  const {method, body} = req;
 
   switch (method) {
     case 'GET':
       return res.status(200).json('getting all notes');
       case 'POST':
-        return res.status(201).json('note created');
+        const {title, content} = body;
+        const query = 'INSERT INTO notes(title, content) VALUES ($1, $2) RETURNING *';
+        const values = [title, content];
+        const response = await connectDB.query(query, values);
+        return res.status(201).json(response.rows[0]);
     default:
       return res.status(400).json('method not allowed')
   }
